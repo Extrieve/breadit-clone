681a61c854853efb746db9ddeb4cc95ed412af99
Nick <nicolas.cinera@gmail.com>
2023-09-17 14:18:21 -0400

    Setting up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/components/ui/Navbar';
 import { cn } from '@/lib/utils';
 import '@/styles/globals.css';
 import { Inter } from 'next/font/google';
@@ -18,7 +19,9 @@ export default function RootLayout({
     <html lang='en' className={cn('bg-white text-slate-900 antialiased light', inter.className)}>
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
         <Navbar />
+        <div className="container max-w-7xl mx-auto h-full pt-12">
         {children}
+        </div>
       </body>
     </html>
   );
