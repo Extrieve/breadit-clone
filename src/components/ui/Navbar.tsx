0feb18a82c2f5d5841a221c3048f0ebe2c7b81b0
Extrieve <nicolas.cinera@gmail.com>
2023-09-24 15:22:09 -0400

    Button Variants are crazy

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Icons } from "../Icons";
+import { buttonVariants } from "./Button";
 
 const Navbar = () => {
   return (<div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-10 py-2">
@@ -10,6 +11,7 @@ const Navbar = () => {
       <p className="hidden text-zinc-700 text-sm font-medium md:block">Breadit</p>
 
       {/* Search */}
+     <Link href={'/sign-in'} className={buttonVariants()}>Sign In</Link> 
     </div>
   </div>);
 };
